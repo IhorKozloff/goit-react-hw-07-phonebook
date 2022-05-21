3c036b2ef5c4d146f9662c979dabbd92e9d4e710
IhorKozloff <ik.eyeshield21@gmail.com>
2022-05-21 21:25:51 +0300

    add contact hook +

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,40 +1,9 @@
 import {ContactsForm} from 'components/Form/Form';
-import React, { useState, useEffect } from "react";
-import { useGetContactsQuery, useAddContactMutation } from "Redux/contactsSlice";
+import { useAddContact } from 'Hooks/useAddContact';
 
 export const SearchBar = () => {
-    const [newContact, setNewContact] = useState('');
-    const { data } = useGetContactsQuery('', {skip: newContact === ''});
-
-
-    const [addContact] = useAddContactMutation();
-
-
-    useEffect(() => {
-        if (!data || newContact === '') {
-            return
-        }
-
-        const existingContact = data.find(item => item.name.toLowerCase() === newContact.name.toLowerCase())
-        const onAddContacts = async (contact) => {
-            try {
-                await addContact(contact);
-            } catch (error) {
-                console.log(error)
-            }
-        }
-
-
-
-        if (existingContact) {
-            alert(`${newContact.name} is already in contacts`);
-        } else {
-            onAddContacts(newContact)
-        }
-
-        return setNewContact('');
-    },[addContact, data, newContact])
-
+    
+    const { setNewContact } = useAddContact();
 
     return (
         <ContactsForm setNewContact={setNewContact}/>
